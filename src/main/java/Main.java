4d4b030afe342d5821e02b7b40b4adf47aa4cca5
bokang03 <bogang12345@gmail.com>
2026-02-05 15:49:26 +0900

    출력을 모니터가 아닌 별도의 문자열로 받는 방법

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,32 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-//        Scanner scan = new Scanner(System.in);
-//        Scanner scan = new Scanner("aaa\nbbb\nddd");
+        lab2();
+    }
+
+    private static void lab2() {
+        PrintStream ORIGINAL_OUT = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream((byteArrayOutputStream));
+
+        System.setOut(printStream);
+
+        // 여기는 안나옴 (byteArrayOutputStream에 쌓이고 있음)
+        System.out.println("hihi");
+        System.out.println("bye");
+
+        System.setOut(ORIGINAL_OUT); // 모티터로 출력 복원
+
+        String result = byteArrayOutputStream.toString();
+        System.out.println(result);
+    }
+
+    public static void lab1(){
         Scanner scan = new Scanner("""
                 등록
                 과거에 등록해라
