6e720843c412769a8178ef6f9765f8380334ea93
bokang03 <bogang12345@gmail.com>
2026-02-06 12:18:19 +0900

    10강 rgb

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,18 +1,32 @@
 import java.util.Scanner;
 
 public class App {
-    //  clearSetOutToByteArray 모니터로 볼 때만 사용
 
-    private Scanner scanner;
+    private Scanner sc;
 
-    public App(Scanner scanner){
-        this.scanner = scanner;
+    public App(Scanner sc) {
+        this.sc = sc;
     }
 
-    public void run(){
+    public void run() {
         System.out.println("== 명언 앱 ==");
-        System.out.println("명령) ");
-        System.out.println("명언 : ");
-        System.out.println("작가 : "); // ouputStream에 쌓인다.
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String saying = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
+                }
+                case "종료" -> {
+                    return;
+                }
+            }
+        }
+
     }
 }
