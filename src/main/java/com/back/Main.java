67387d816dddefade4882ce8fcf26612855089cb
bokang03 <bogang12345@gmail.com>
2026-02-09 10:28:52 +0900

    24강 해당 키워드가 포함된 목록 출력 rgb

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -4,44 +4,47 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
-
         lab2();
     }
 
-    private static void lab2() {
+    public static void lab2() {
+
         PrintStream ORIGINAL_OUT = System.out;
 
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream((byteArrayOutputStream));
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
 
         System.setOut(printStream);
 
-        // 여기는 안나옴 (byteArrayOutputStream에 쌓이고 있음)
         System.out.println("hihi");
-        System.out.println("bye");
-        System.out.println("ok");
+        System.out.println("byebye");
+        System.out.print("ok");
 
-        System.setOut(ORIGINAL_OUT); // 모티터로 출력 복원
+        System.setOut(ORIGINAL_OUT); // 모니터로 출력 복원
 
         String result = byteArrayOutputStream.toString();
+
         System.out.println(result);
     }
 
-    public static void lab1(){
+    public static void lab1() {
         Scanner scan = new Scanner("""
                 등록
-                과거에 등록해라
-                작자 미상
+                과거에 집착하지 마라.
+                작자미상
                 """);
 
         String str1 = scan.nextLine();
         String str2 = scan.nextLine();
         String str3 = scan.nextLine();
 
+
         System.out.println(str1);
         System.out.println(str2);
         System.out.println(str3);
+
     }
 }
